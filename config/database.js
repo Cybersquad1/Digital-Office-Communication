d0cf9ec8a7a0822ed4fdb3e375eed492798450f8
Akhilesh Anand <akhileshmalini@gmail.com>
2017-01-30 12:50:08 +0530

    First commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://doc2017:doc2017@ds056419.mlab.com:56419/docdb' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
